6a51c6e3b80eb80a1e9feecd1955029af4ee759d
Eduardo Hernández <eduardo.h@cinemasapp.co>
2018-10-05 01:44:41 -0500

    Updates

---
+++ precache-manifest.01d49635f1a2c7d40169e505404418ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "996b5dca19f3a6eceaf6e973b67eaa57",
+    "url": "/anime-page/static/media/wall.996b5dca.png"
+  },
+  {
+    "revision": "3db51028bc3f1ae9fff1fa3ca6ca76ec",
+    "url": "/anime-page/static/media/loading.3db51028.gif"
+  },
+  {
+    "revision": "2c24f9ae46929d77e4e0",
+    "url": "/anime-page/static/js/runtime~main.2c24f9ae.js"
+  },
+  {
+    "revision": "b51d83180ab70625c1ea",
+    "url": "/anime-page/static/js/main.b51d8318.chunk.js"
+  },
+  {
+    "revision": "15e8cbe0142e1b4ffff0",
+    "url": "/anime-page/static/js/1.15e8cbe0.chunk.js"
+  },
+  {
+    "revision": "b51d83180ab70625c1ea",
+    "url": "/anime-page/static/css/main.d5928912.chunk.css"
+  },
+  {
+    "revision": "1e968b42688b09af6b284154dd590b48",
+    "url": "/anime-page/index.html"
+  }
+];
